a4254bd5e8e14cbfd75c0062f1fc97d2510ab6a5
glacyalwolf <ismaelromero1999@gmail.com>
2021-09-21 10:02:36 +0200

    updateArrival completedç

---
+++ List1/IpdateArrival.py
@@ -0,0 +1,12 @@
+def update_arrrival(h, m, d):
+    while(d>1):
+        m=m+1;
+        if(m>=60):
+            h=h+1;
+            m=1;
+        if(h>23):
+            h=0;
+        d=d-1;
+    return(h, m)
+
+print(str(update_arrrival(10,45,20)))
