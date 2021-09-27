9d24f2f3ffe83fe5d4da993c524628d14e81f542
glacyalwolf <ismaelromero1999@gmail.com>
2021-09-27 11:35:53 +0200

    correcc date inici

---
+++ List1/correctDate.py
@@ -0,0 +1,48 @@
+def leapyear(y):
+    '''
+    >>> leapyear(1999)
+    False
+    >>> leapyear(1968)
+    True
+    >>> leapyear(2000)
+    True
+    >>> leapyear(1800)
+    False
+    '''
+        
+    comprovante=True;
+    if(y%100!=0 and y%4==0 ):
+        comprovante=True;
+
+    elif(y%100==0 and (y//100)%4==0):
+        comprovante=True;
+    
+    else:
+        comprovante=False;
+    return comprovante
+
+
+def correct_date(d,m,y):
+    '''
+    >>> correct_date(30, 11, 1971)
+    True
+    >>> correct_date(6, 4, 1971)
+    True
+    >>> correct_date(4, 8, 2001)
+    True
+    >>> correct_date(29, 2, 2001)
+    False
+    >>> correct_date(32, 11, 2005)
+    False
+    >>> correct_date(30, 11, 2004)
+    True
+    >>> correct_date(-20, 15, 2000)
+    False
+    '''
+    if
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
