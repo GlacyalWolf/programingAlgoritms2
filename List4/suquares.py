3c70c3c607d698f8aeb7aed14614a6308c3dc84f
glacyalwolf <ismaelromero1999@gmail.com>
2021-10-19 20:50:08 +0200

    squares start

---
+++ List4/suquares.py
@@ -0,0 +1,17 @@
+from sys import stdin;
+
+def squares():
+    
+    while(stdin!=""):
+        int(stdin.readlines());
+
+    
+    for n in stdin:
+        for i in range(n):
+            for i in range(n):
+                print(n,end="")
+            print("");
+        print("");
+
+
+squares()
