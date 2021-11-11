c8fe764a4a26a4a57ec4e97989f04f35956102e0
glacyalwolf <ismaelromero1999@gmail.com>
2021-11-11 13:07:27 +0100

    wordsBetweenWords.py done :)

---
+++ List5/wordsBetweenWords.py
@@ -0,0 +1,12 @@
+string=input();
+string=string.split();
+
+try:
+    if(string.index("beginning")<string.index("end") and ("beginning","end " in string)):
+        print(string.index("end")-string.index("beginning")-1)
+    else:
+        print("Error")
+
+
+except:
+    print("Error")
