91b347b22884b714852cddf8478542f8c720820a
glacyalwolf <ismaelromero1999@gmail.com>
2022-02-24 12:39:28 +0100

    freqsufix.py finished

---
+++ List7/freqSufix.py
@@ -0,0 +1,20 @@
+from easyinput import read
+
+word=read(str)
+suflist={}
+while word is not None:
+    
+    suf=word[-3:]
+
+    if len(word)>2:
+        if(suf in suflist):
+            suflist[suf]+=[word]
+        else:
+            suflist[suf]=[word]
+    word=read(str)
+
+
+for i in sorted(suflist):
+    lenlist=len(suflist[i])
+    if (lenlist>1):
+        print(f"{i}: {lenlist} "+" ".join(suflist.get(i)))
